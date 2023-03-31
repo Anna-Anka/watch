bce086a84abb259142f0cc126f34460dbe3371aa
Anna-Anka <anjbekrenewa@gmail.com>
2023-03-31 09:04:35 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -323,7 +323,6 @@ const checkoutForm = () => {
         const blockId = el.getAttribute('data-goto').substr(1);
         const block = document.getElementById(blockId).closest('.checkout-form__list');
         block.classList.add('checkout-form__list--collapsed');
-        console.log(block);
       });
     });
   }
@@ -670,23 +669,55 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 const smoothScroll = () => {
   const menuLinks = document.querySelectorAll('a[data-goto]');
+  const body = document.body;
 
   if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
       menuLink.addEventListener('click', onMenuLinkClick);
     });
 
+    const disableScroll = () => {
+      body.style.position = 'relative';
+      let pagePosition = window.scrollY;
+      body.dataset.position = pagePosition;
+      body.style.top = -pagePosition + 'px';
+    };
+
+    const enableScroll = () => {
+      body.style.position = 'static';
+      let pagePosition = parseInt(body.dataset.position, 10);
+      body.style.top = 'auto';
+      window.scroll({
+        top: pagePosition,
+        left: 0
+      });
+      body.removeAttribute('data-position');
+    };
+
     function onMenuLinkClick(e) {
       const menuLink = e.target;
 
       if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
         const gotoBlock = document.querySelector(menuLink.dataset.goto);
-        const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
-        window.scrollTo({
-          top: gotoBlockValue,
-          behavior: "smooth"
-        });
-        e.preventDefault();
+
+        const delay = delayInms => {
+          return new Promise(resolve => setTimeout(resolve, delayInms));
+        };
+
+        disableScroll();
+
+        const sample = async () => {
+          let delayres = await delay(350);
+          const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+          window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+          });
+          e.preventDefault();
+        };
+
+        sample();
+        setTimeout(() => enableScroll());
       }
     }
   }
